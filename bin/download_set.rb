46e0ac3cf4e6048be48373770a4959ab039f529a
corinthian <ianyh@mit.edu>
2010-08-06 04:09:07 -0400

    new data model to retain set information and handle images better

--- bin/download_set.rb
+++ bin/download_set.rb
@@ -30,85 +30,95 @@ end
 
 def download(options)
   agent = Mechanize.new
-  page = agent.get options[:url]
-  downloaded_names = Set.new
-  cards = []
 
-  page.search('html body table tr td table tr td table').each do |color_table|
-    color_table.search('tr td table').each do |card_table|
-      name = card_table.search('tr[1] td[1]').text.strip
-      name.slice!(0) if name =~ /^\*/
-      if downloaded_names.include? name
-        next
-      else
-        downloaded_names.add name
-      end
+  if options[:url] and options[:set]
+    page = agent.get options[:url]
+    downloaded_names = Set.new
+    cards = []
 
-      mana_cost = ''
-      converted_mana_cost = 0
-      card_table.search('tr[1] td[2] img').each do |mana_el|
-        if mana_el['alt'] =~ /\d+/
-          converted_mana_cost += mana_el['alt'].to_i
+    page.search('html body table tr td table tr td table').each do |color_table|
+      color_table.search('tr td table').each do |card_table|
+        name = card_table.search('tr[1] td[1]').text.strip
+        name.slice!(0) if name =~ /^\*/
+        if downloaded_names.include? name
+          next
         else
-          converted_mana_cost += 1
+          downloaded_names.add name
         end
-        mana_cost += mana_el['alt'].upcase
-      end
 
-      type = card_table.search('tr[2] td[1]').text.strip
-
-      rarity = card_table.search('tr[2] td[2] img')[0]['alt'][0,1].upcase
-      if rarity == 'B'
-        rarity = 'L'
-      end
-
-      text = Sanitize.clean(card_table.search('tr[3] td')[0].inner_html, Sanitize::Config::RELAXED)
-      text.gsub!(/<img.+?\/>/) do |match|
-        alt = match[/alt=(.+?)\s/]
-        if alt =~ /\{\w\}/
-          alt = alt[/\{\w\}/][1,1]
-        else
-          alt = alt[/\d/]
+        mana_cost = ''
+        converted_mana_cost = 0
+        card_table.search('tr[1] td[2] img').each do |mana_el|
+          if mana_el['alt'] =~ /\d+/
+            converted_mana_cost += mana_el['alt'].to_i
+          else
+            converted_mana_cost += 1
+          end
+          mana_cost += mana_el['alt'].upcase
         end
-        alt
-      end
-      text.gsub!(/<br \/>/, "\n")
-      text.gsub!(/&#13;/, '')
-      text = text.strip
-      text = Sanitize.clean(text)
-
-      pt = card_table.search('tr[4] td[2]').text.strip
-      power = 0
-      toughness = 0
-      unless pt == ''
-        if power =~ /\//
-          power = pt[/^\d+/] || power
+        
+        type = card_table.search('tr[2] td[1]').text.strip
+        
+        rarity = card_table.search('tr[2] td[2] img')[0]['alt'][0,1].upcase
+        if rarity == 'B'
+          rarity = 'L'
         end
-        toughness = pt[/\d+$/] || toughness
+        
+        text = Sanitize.clean(card_table.search('tr[3] td')[0].inner_html, Sanitize::Config::RELAXED)
+        text.gsub!(/<img.+?\/>/) do |match|
+          alt = match[/alt=(.+?)\s/]
+          if alt =~ /\{\w\}/
+            alt = alt[/\{\w\}/][1,1]
+          else
+            alt = alt[/\d/]
+          end
+          alt
+        end
+        text.gsub!(/<br \/>/, "\n")
+        text.gsub!(/&#13;/, '')
+        text = text.strip
+        text = Sanitize.clean(text)
+        
+        pt = card_table.search('tr[4] td[2]').text.strip
+        power = 0
+        toughness = 0
+        unless pt == ''
+          if power =~ /\//
+            power = pt[/^\d+/] || power
+          end
+          toughness = pt[/\d+$/] || toughness
+        end
+        
+        cards += [[name, mana_cost, converted_mana_cost, power, toughness, rarity, type, text]]
       end
+    end
 
-      cards += [[name, mana_cost, converted_mana_cost, power, toughness, rarity, type, text]]
+    puts "found #{cards.size} cards"
+
+    FasterCSV.open("../res/sets/#{options[:set]}.csv", File::CREAT|File::WRONLY) do |csv|
+      cards.each do |card|
+        csv << card
+      end
     end
   end
 
-  puts "found #{cards.size} cards"
-
-  page = agent.get options[:image_url]
-  page.search('img').each do |img|
-    name = img['alt']
-    if downloaded_names.include? name
-      url = img['src']
-      url = 'http://gatherer.wizards.com/' + url.gsub('../', '')
-      options[:image_map][filename_from_name(name)] = url
+  if options[:image_url]
+    page = agent.get options[:image_url]
+    puts page.search('img').each {|foo|}
+    page.search('img').each do |img|
+      next unless img
+      name = img['alt']
+      next unless name
+      
+      if options[:url] and options[:set] and downloaded_names.include? name or options[:image_url]
+        url = img['src']
+        url = 'http://gatherer.wizards.com/' + url.gsub('../', '')
+        options[:image_map][filename_from_name(name)] = url
+      end
     end
+    write_image_map(options[:image_map])
   end
-  write_image_map(options[:image_map])
   
-  FasterCSV.open("../res/sets/#{options[:set]}.csv", File::CREAT|File::WRONLY) do |csv|
-    cards.each do |card|
-      csv << card
-    end
-  end
 end
 
 if __FILE__ == $0
@@ -121,8 +131,8 @@ if __FILE__ == $0
     opts.on('-u', '--spoiler-url', String, 'url to grab card information from') do |u|
       options[:url] = u
     end
-    opts.on('-i', '--images-url', String, 'url to grab card image urls from') do |u|
-      options[:image_url] = u
+    opts.on('-i', '--images-url URL', String, 'url to grab card image urls from') do |s|
+      options[:image_url] = s
     end
     opts.on_tail('-h', '--help', 'show this message') do
       puts opts
@@ -131,11 +141,14 @@ if __FILE__ == $0
   end
 
   opts.parse!(ARGV)
-
-  unless options[:url] and options[:set] and options[:image_url]
-    puts opts
-    exit
+  
+  unless options[:image_url]
+    puts 'no image url'
+    unless (options[:url] and options[:set])
+      puts opts
+      exit
+    end
   end
 
-  download(options)
+  download(options.merge({:image_map => load_image_map}))
 end
