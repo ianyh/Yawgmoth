427a1d36bafbc6bd0ddcae387eb866349219c6ce
corinthian <ianyh@mit.edu>
2010-09-26 14:06:28 -0400

    rewrote download set for gatherer

--- bin/download_set.rb
+++ bin/download_set.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/env ruby
+
 require 'rubygems'
 require 'mechanize'
 require 'optparse'
@@ -5,13 +7,6 @@ require 'sanitize'
 require 'set'
 require 'pp'
 require 'fastercsv'
-
-def load_image_map
-  image_map_file = '../res/card-image-map'
-  FasterCSV.read(image_map_file, :col_sep => "\t").inject({}) do |img_map, pair|
-    img_map.merge({ pair[0] => pair[1] })
-  end
-end
 
 def write_image_map(image_map)
   image_map_file = '../res/card-image-map'
@@ -29,78 +24,102 @@ def filename_from_name(card_name)
 end
 
 def download(options)
-  agent = Mechanize.new
+  def clean_row(row)
+    return row.content.gsub(/ +/, ' ').gsub(/\n+/,"\n\n").gsub(/\r+/, '').gsub(/(\s)\s*/, '\1').gsub("\342\200\224", "-").chomp
+  end
 
+  def process_card(card)
+    processed = card.clone
+
+    # convert power and toughness
+    # have to deal with loyalty on planeswalkers
+    if processed[:pt] =~ /\//
+      processed[:pt] = [processed[:pt][/\((.+)\//, 1],
+                        processed[:pt][/\/(.+?)\)/, 1]]
+    elsif processed[:pt] =~ /\(/
+      processed[:pt] = [nil, processed[:pt][/\((.+?)\)/, 1]]
+    else
+      processed[:pt] = [nil, nil]
+    end
+
+    # calculate converted mana cost
+    #
+    # if there are parentheses, the cost has split costs like (W/U)
+    # each split cost is counted as 1 in converted mana cost; might be
+    # weirdness there with things like (2/U)
+    if processed[:cost]
+      if processed[:cost] =~ /\(/
+        processed[:converted_cost] = processed[:cost].scan(/\(.+?\)/).length
+      else
+        processed[:converted_cost] = 0
+      end
+      
+      processed[:converted_cost] += processed[:cost].scan(/[WUBRG]/).length +
+        processed[:cost].gsub(/[WUBRGX]/, '').to_i
+    else
+      processed[:converted_cost] = 0
+    end
+
+    # convert rarity string to rarity character
+    if processed[:rarity] =~ /Mythic Rare/
+      processed[:rarity] = 'M'
+    elsif processed[:rarity] =~ /Rare/
+      processed[:rarity] = 'R'
+    elsif processed[:rarity] =~ /Uncommon/
+      processed[:rarity] = 'U'
+    elsif processed[:rarity] =~ /Common/
+      processed[:rarity] = 'C'
+    elsif processed[:rarity] =~ /Basic/
+      processed[:rarity] = 'L'
+    else
+      puts "unknown rarity: #{processed[:rarity]}"
+      processed[:rarity] = nil
+    end
+
+    processed
+  end
+  
+  agent = Mechanize.new
+  
   if options[:url] and options[:set]
     page = agent.get options[:url]
     downloaded_names = Set.new
     cards = []
+    card = {}
 
-    page.search('html body table tr td table tr td table').each do |color_table|
-      color_table.search('tr td table').each do |card_table|
-        name = card_table.search('tr[1] td[1]').text.strip
-        name.slice!(0) if name =~ /^\*/
-        if downloaded_names.include? name
-          next
-        else
-          downloaded_names.add name
-        end
+    page.search('div.textspoiler table tr').each do |data_row|
+      content = clean_row(data_row)
+      attr_sym = nil
+      if content =~ /Name:/
+        attr_sym = :name
+      elsif content =~ /Cost:/
+        attr_sym = :cost
+      elsif content =~ /Type:/
+        attr_sym = :type
+      elsif content =~ /Pow\/Tgh:|Loyalty:/
+        attr_sym = :pt
+      elsif content =~ /Rules Text:/
+        attr_sym = :text
+      elsif content =~ /Rarity:/
+        attr_sym = :rarity
+      end
 
-        mana_cost = ''
-        converted_mana_cost = 0
-        card_table.search('tr[1] td[2] img').each do |mana_el|
-          if mana_el['alt'] =~ /\d+/
-            converted_mana_cost += mana_el['alt'].to_i
-          else
-            converted_mana_cost += 1
-          end
-          mana_cost += mana_el['alt'].upcase
-        end
-        
-        type = card_table.search('tr[2] td[1]').text.strip
-        
-        rarity = card_table.search('tr[2] td[2] img')[0]['alt'][0,1].upcase
-        if rarity == 'B'
-          rarity = 'L'
-        end
-        
-        text = Sanitize.clean(card_table.search('tr[3] td')[0].inner_html, Sanitize::Config::RELAXED)
-        text.gsub!(/<img.+?\/>/) do |match|
-          alt = match[/alt=(.+?)\s/]
-          if alt =~ /\{\w\}/
-            alt = alt[/\{\w\}/][1,1]
-          else
-            alt = alt[/\d/]
-          end
-          alt
-        end
-        text.gsub!(/<br \/>/, "\n")
-        text.gsub!(/&#13;/, '')
-        text = text.strip
-        text = Sanitize.clean(text)
-        
-        pt = card_table.search('tr[4] td[2]').text.strip
-        power = 0
-        toughness = 0
-        unless pt == ''
-          if pt =~ /\//
-            power = pt[/^\d+/] || power
-          end
-          toughness = pt[/\d+$/] || toughness
-        end
-        
-        cards += [[name, mana_cost, converted_mana_cost, power, toughness, rarity, type, text]]
+      if attr_sym.nil?
+        cards.push process_card(card)
+        card.clear
+      else
+        card[attr_sym] = content[/:\n((\n|.)+)$/, 1]
       end
     end
-
+    
     puts "found #{cards.size} cards"
 
     FasterCSV.open("../res/sets/#{options[:set]}.csv", File::CREAT|File::WRONLY) do |csv|
       cards.each do |card|
-        csv << card
+        csv << [card[:name], card[:cost], card[:converted_cost]] + card[:pt] + [card[:rarity], card[:type], card[:text]]
       end
-    end
-  end  
+    end    
+  end
 end
 
 if __FILE__ == $0
